ceff1504dd78a6b2c08e839407e18b39f63566cb
Santiago Escalante <hola@sanntu.com>
2014-02-04 19:01:58 -0300

    Primer dia de trabajo en el proyecto

---
+++ teaser/brokers.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<!--[if IE 8]><html class="no-js lt-ie9" lang="en"><![endif]-->
+<!--[if gt IE 8]><!--><html class="no-js" lang="en"><!--<![endif]-->
+<head>
+   <!-- Set the viewport width to device width for mobile -->
+   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+   <meta name="format-detection" content="telephone=no" />
+   
+   <link type="text/plain" rel="author" href="humans.txt" />
+
+   <meta charset="utf-8" />
+   
+   <title>Metropolis | Broken Registration</title>
+
+   <!-- Favicon -->
+   <link rel="icon" href="favicon.ico" type="image/x-icon" />
+   <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+
+   <!-- Included CSS Files -->
+   <link rel="stylesheet" href="css/styles.css" type="text/css" />
+
+   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+   <!--[if lt IE 9]>
+      <script src="//oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="//oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+      <script src="//oss.maxcdn.com/libs/respond.js/1.4.2/respond.src.js"></script>
+   <![endif]-->
+</head>
+<body>
+   <section class="fullscreen">
+      <a class="close" href="#">Close</a>
+      <div class="wrapper">
+         <div class="registration">
+            <div class="hgroup">
+               <h1>Broker registration<hr /></h1>
+            </div>
+            <form class="form" method="" action="">
+               <fieldset>
+                  <div class="row">
+                     <div class="small-4 medium-4 large-4 columns">
+                        <div class="field name">
+                           <label>*Name</label>
+                           <input name="" class="text" type="text" value="First" />
+                           <input name="" class="text" type="text" value="Last" />
+                        </div>
+                        <div class="field">
+                           <label>*Brokerage company</label>
+                           <input name="" class="text" type="text" />
+                        </div>
+                        <div class="field">
+                           <label>*Email</label>
+                           <input name="" class="text" type="text" />
+                        </div>
+                     </div>
+                     <div class="small-4 medium-4 large-4 columns">
+                        <div class="field">
+                           <label>How did you hear about metropolis ?</label>
+                           <input name="" class="text" type="text" />
+                        </div>
+                        <div class="field">
+                           <button class="form-button">Send</button>
+                           <small>*Required</small>
+                        </div>
+                     </div>
+                     <div class="small-4 medium-4 large-4 columns"></div>
+                  </div>
+               </fieldset>
+            </form>
+         </div>
+      </div>
+   </section>
+</body>
+</html>
