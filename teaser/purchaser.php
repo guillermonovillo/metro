fb1ee5e81f77746a88e6f29d426901607ba850e2
marcoscaceres <marcoscaceres@bridgerconway.com>
2014-02-07 19:06:42 -0300

    commit error de sincro

---
+++ teaser/purchaser.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<!--[if IE 8]><html class="no-js lt-ie9" lang="en"><![endif]-->
+<!--[if gt IE 8]><!--><html class="no-js" lang="en"><!--<![endif]-->
+<head>
+   <!-- Set the viewport width to device width for mobile -->
+   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+   <meta name="format-detection" content="telephone=no" />
+   
+   <link type="text/plain" rel="author" href="humans.txt" />
+
+   <meta charset="utf-8" />
+   
+   <title>Metropolis | Purchaser Registration</title>
+
+   <!-- Favicon -->
+   <link rel="icon" href="favicon.ico" type="image/x-icon" />
+   <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+
+   <!-- Included CSS Files -->
+   <link rel="stylesheet" href="css/styles.css" type="text/css" />
+
+   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+   <!--[if lt IE 9]>
+      <script src="//oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="//oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+      <script src="//oss.maxcdn.com/libs/respond.js/1.4.2/respond.src.js"></script>
+   <![endif]-->
+</head>
+<body>
+   <section class="fullscreen">
+      <a class="close" href="#">Close</a>
+      <div class="wrapper">
+         <div class="registration">
+            <div class="hgroup">
+               <h1>Purchaser registration</h1>
+            </div>
+            <div class="main-content">
+               <form class="form" method="" action="">
+                  <fieldset>
+                     <div class="row">
+                        <div class="medium-4 large-4 columns column-1">
+                           <div class="field field-2">
+                              <label>*Name</label>
+                              <input name="" class="text first" type="text" value="First" />
+                              <input name="" class="text last" type="text" value="Last" />
+                           </div>
+                           <div class="field">
+                              <label>*Address</label>
+                              <input name="" class="text" type="text" />
+                           </div>
+                           <div class="field">
+                              <label>*Email</label>
+                              <input name="" class="text" type="text" />
+                           </div>
+                        </div>
+                        <div class="medium-4 large-4 columns column-2">
+                           <div class="field">
+                              <label>Country</label>
+                              <select name="prettyfied" class="prettyfied">
+                                 <option value="0" selected="selected"></option>
+                                 <option value="1" data-html-text="Argentine">Option One</option>
+                                 <option value="2" data-html-text="Brazil">Option Two</option>
+                                 <option value="3" data-html-text="Uruguay">Option Three</option>
+                                 <option value="4" data-html-text="Chile">Option Five</option>
+                                 <option value="4" data-html-text="Colombia">Option Six</option>
+                                 <option value="4" data-html-text="Peru">Option Seven</option>
+                              </select>
+                           </div>
+                           <div class="field">
+                              <label>*Phone</label>
+                              <input name="" class="text" type="text" />
+                           </div>
+                           <div class="field alt">
+                              <label>How did you hear<br />about metropolis ?</label>
+                              <select name="prettyfied" class="prettyfied">
+                                 <option value="0" selected="selected"></option>
+                                 <option value="1" data-html-text="Bathroom">Option One</option>
+                                 <option value="2" data-html-text="Office">Option Two</option>
+                                 <option value="3" data-html-text="Opera">Option Three</option>
+                                 <option value="4" data-html-text="Listening music">Option Four</option>
+                              </select>
+                           </div>
+                        </div>
+                        <div class="medium-4 large-4 columns column-3">
+                           <div class="field">
+                              <label>*Represented by Brokers?</label>
+                              <a class="radio-button selected" href="#">Yes</a>
+                              <input name="" type="hidden" />
+                              <a class="radio-button" href="#">No</a>
+                              <input name="" type="hidden" />
+                           </div>
+                           <div class="field field-2">
+                              <label>*Broker's name</label>
+                              <input name="" class="text first" type="text" />
+                              <input name="" class="text last" type="text" />
+                           </div>
+                           <div class="field field-2">
+                              <label>*Broker's Co. Contact Number</label>
+                              <input name="" class="text first" type="text" />
+                              <input name="" class="text last" type="text" />
+                           </div>
+                           <div class="field">
+                              <label>*Broker's Co. email</label>
+                              <input name="" class="text" type="text" />
+                           </div>
+                           <div class="field field-button">
+                              <button class="form-button">Send</button><br /><small>*Required</small>
+                           </div>
+                        </div>
+                     </div>
+                  </fieldset>
+               </form>
+            </div>
+         </div>
+      </div>
+   </section>
+   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js"></script>
+   <script src="js/prettyfied-select.js" type="text/javascript"></script>
+</body>
+</html>
