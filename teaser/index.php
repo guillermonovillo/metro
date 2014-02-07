ac973d0d34bb1c27ad8de08aa2c21df0109c998b
Santiago Escalante <hola@sanntu.com>
2014-02-07 19:04:46 -0300

    Comienzo del desarrollo de la Home.

---
+++ teaser/index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<!--[if IE 8]><html class="no-js lt-ie9" lang="en"><![endif]-->
+<!--[if gt IE 8]><!--><html class="no-js" lang="en"><!--<![endif]-->
+<head>
+   <!-- Set the viewport width to device width for mobile -->
+   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+   <meta name="format-detection" content="telephone=no" />
+   
+   <link type="text/plain" rel="author" href="humans.txt" />
+
+   <meta charset="utf-8" />
+   
+   <title>Metropolis | Home page</title>
+
+   <!-- Favicon -->
+   <link rel="icon" href="favicon.ico" type="image/x-icon" />
+   <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+
+   <!-- Included CSS Files -->
+   <link rel="stylesheet" href="css/styles.css" type="text/css" />
+
+   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+   <!--[if lt IE 9]>
+      <script src="//oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="//oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+      <script src="//oss.maxcdn.com/libs/respond.js/1.4.2/respond.src.js"></script>
+   <![endif]-->
+</head>
+<body class="home">
+   <div class="content">
+      <header class="header">
+         <h1>Metropolis, Los Angeles</h1>
+         <h2>Coming soon to<br />Downtown Los Angeles</h2>
+      </header>
+      <nav class="nav">
+         <ul>
+            <li>Language</li>
+            <li><a href="#">En</a></li>
+            <li><a href="#">Ch</a></li>
+         </ul>
+      </nav>
+      <div class="main-content">
+         <p>Please select one:</p>
+         <a href="#">Prospective purchaser</a>
+         <a href="#">Broker</a>
+      </div>
+      <footer class="footer">
+         <p>Developed by<br />Greenland USA</p>
+         <p>Metropolis - All right reserved.</p>
+         <p>Exclusive Sales & Marketing</p>
+         <ul class="">
+            <li><a href="#">Disclaimer</a></li>
+            <li><a href="#">Site Credits</a></li>
+         </ul>
+      </footer>
+   </div>
+</body>
+</html>
