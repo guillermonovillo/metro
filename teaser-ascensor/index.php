579e54272fd8920d23fe21894f4e15bb8b276923
marcoscaceres <marcoscaceres@bridgerconway.com>
2014-04-24 20:01:09 -0300

    nuevo logo

--- teaser-ascensor/index.php
+++ teaser-ascensor/index.php
@@ -18,6 +18,7 @@
 
    <!-- Included CSS Files -->
    <link rel="stylesheet" href="css/styles.css" type="text/css" />
+   <link rel="stylesheet" href="css/styles_phase_2.css" type="text/css" />
 
    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
