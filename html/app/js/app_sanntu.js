5383732422b1acd0e3f90f63f5b2de805ee46782
Santiago Escalante <hola@sanntu.com>
2014-08-28 18:04:44 -0300

    Contact us
    Se unifican todas las pantallas de contact us

--- html/app/js/app_sanntu.js
+++ html/app/js/app_sanntu.js
@@ -21,7 +21,10 @@ $(document).ready(function() {
       });
 
       // contact us > *represented by brokers?
-      $('.radio-button').click(function(e){
+      var broker_wrapper = $(".has-broker-wrapper");
+      var broker_input_text = $(".has-broker-wrapper .text");
+
+      $('.radio-button').on('click tap', (function(e){
          e.preventDefault();
          var $this = $(this);
          if (!$this.hasClass('selected')) {
@@ -29,16 +32,105 @@ $(document).ready(function() {
                .addClass('selected')
                .siblings('a').removeClass('selected');
          }
-      });
+      }));
 
       $(".has-broker-yes").on('click tap', function(e){
          e.preventDefault();
-         $(".has-broker").addClass('visible');
+         broker_wrapper.addClass('visible');
+         broker_input_text.removeClass('skip');
       });
 
       $(".has-broker-no").on('click tap', function(e){
          e.preventDefault();
-         $(".has-broker").removeClass('visible'); 
+         broker_wrapper.removeClass('visible');
+         broker_input_text.addClass('skip');
+      });
+
+      // contact us > thanks
+      $('.thanks .close').on('click tap', function(e){
+         e.preventDefault();
+         $(this).parents('.thanks').removeClass('visible');
+      });
+
+      // contact us > form validation
+      $.validator.setDefaults({
+
+         onkeyup: false,
+
+         errorPlacement: function(error, element){
+            $(element).parent().addClass('error');
+            $(element).attr('placeholder', error.text());
+         },
+
+         unhighlight: function( element, errorClass, validClass ) {
+            $(element).parent().removeClass('error');
+         },
+
+         submitHandler: function (form) {
+
+            $(".thanks").addClass('visible');
+            
+            // var $form = $(form);
+            
+            // $.ajax({
+            //    url: $form.attr('action'),
+            //    type: $form.attr('method'),
+            //    data: $form.serialize(),
+            //    dataType: 'json',
+            //    success: function (response) {
+            //       if (response.ok) {
+            //          $(".thanks").addClass('visible');
+            //       } else {
+            //          alert ('Please, try again later');
+            //       }
+            //    }
+            // });
+         }
+
+      });
+
+      $("#purchaser-form").validate({
+
+         ignore: '.skip',
+
+         messages: {
+
+            first_name: { required: 'Required' },
+            last_name: { required: 'Required' },
+            address: { required: 'Required' },
+            phone: { required: 'Required' },
+            email: { required: 'Required' },
+            
+            broker_first_name: { required: 'Required' },
+            broker_last_name: { required: 'Required' },
+            broker_contact_number_one: { required: 'Required' },
+            broker_contact_number_two: { required: 'Required' },
+            broker_email: { required: 'Required' }
+         }
+
+      });
+
+      $("#broker-form").validate({
+
+         messages: {
+            first_name: { required: 'Required' },
+            last_name: { required: 'Required' },
+            brokers_company_name: { required: 'Required' },
+            email: { required: 'Required' }
+         }
+
+      });
+
+      $("#press-form").validate({
+
+         messages: {
+            first_name: { required: 'Required' },
+            last_name: { required: 'Required' },
+            email: { required: 'Required' },
+            publication: { required: 'Required' },
+            request: { required: 'Required' }
+         }
+         
       });
 
    });
