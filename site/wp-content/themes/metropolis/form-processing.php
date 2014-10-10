791baad711e6dcbfdf05eb10c83f13bc041cb599
msilioni <max.silioni@gmail.com>
2014-10-10 19:46:08 -0300

    Intregración formulario 3

---
+++ site/wp-content/themes/metropolis/form-processing.php
@@ -0,0 +1,34 @@
+<?php
+ 
+$form_type = 'Press';
+$form_subject = 'Metropolis Contact Form Submission: PRESS';
+
+$header = "From: info@metropolislosangeles.com \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain;charset=utf-8 \r\n";
+
+$message = "Message sent from the " . $form_type . " webform: \r\n";
+$message .= "First Name: " . $_POST['first_name'] . " \r\n";
+$message .= "Last Name: " . $_POST['last_name'] . " \r\n";
+
+if ( isset( $_POST['email'] ) && !empty( $_POST['email'] ) )
+	$message .= "Email: " . $_POST['email'] . " \r\n";
+
+if ( isset( $_POST['publication'] ) && !empty( $_POST['publication'] ) )
+	$message .= "publication: " . $_POST['publication'] . " \r\n";
+
+// $to = "paulette.kam@bwr-pr.com";
+$to2 = "ncornaggia@bridgerconway.com";
+
+$subject = $form_subject;
+
+$response = new stdClass;
+$response->ok = mail($to2, $subject, $message, $header);
+
+header("Location: ".$_POST['returnURL']);
+die();
+// echo json_encode($response);
+// die();
+
+?>
